1f4941b235416f0fd31f2f0cf23f733eeb962773
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-12-03 19:35:15 +0100

    addd attack and some things from takeDamage

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -6,12 +6,13 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 16:19:20 by meghribe          #+#    #+#             */
-/*   Updated: 2025/12/03 18:59:27 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/12/03 19:34:39 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include <iostream>
+#include <string>
 
 ClapTrap::ClapTrap(): name("Default"), hit_points(10), energy_points(10), attack_damage(0)
 {
@@ -21,6 +22,11 @@ ClapTrap::ClapTrap(): name("Default"), hit_points(10), energy_points(10), attack
 ClapTrap::ClapTrap(std::string name): name(name), hit_points(10), energy_points(10), attack_damage(0)
 {
 	std::cout << "ClapTrap with name parameter constructor called" << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "Destructor for " << this->name << " called" << std::endl;
 }
 
 ClapTrap::ClapTrap(const ClapTrap& rhs): 
@@ -37,8 +43,30 @@ ClapTrap& ClapTrap::operator=(const ClapTrap& rhs)
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &rhs)
 	{
-		// TODO: ADD MORE
 		this->name = rhs.name;
+		this->hit_points = rhs.hit_points;
+		this->energy_points = rhs.energy_points;
+		this->attack_damage = rhs.attack_damage;
 	}
 	return (*this);
 }
+
+void	ClapTrap::attack(const std::string& target)
+{
+	std::string	msg;
+
+	if (this->hit_points != 0 && this->energy_points != 0)
+	{
+		msg = "ClapTrap " + this->name + " attacks " + target + ", causing";
+		std::cout << msg << (int)this->attack_damage << std::endl << " points of damage!";
+		this->energy_points--;
+	}
+	else 
+		std::cout << CANNOT_ATTACK << std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	(void)amount;
+}
+
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,12 +6,14 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 16:19:13 by meghribe          #+#    #+#             */
-/*   Updated: 2025/12/03 16:56:08 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/12/03 19:12:42 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
 # define CLAPTRAP_HPP
+# define CANNOT_ATTACK \
+	"ClapTrap can't do anything if it has no hit points or energy points left"
 
 #include <iostream>
 
@@ -29,11 +31,9 @@ class ClapTrap
 		~ClapTrap();
 		ClapTrap& operator=(const ClapTrap& rhs);
 		ClapTrap(const ClapTrap &rhs);
-		/*
 		void attack(const std::string& target);
 		void takeDamage(unsigned int amount);
 		void beRepaired(unsigned int amount);
-		*/
 };
 
 #endif
