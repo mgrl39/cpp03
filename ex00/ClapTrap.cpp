602e5ecbfbd1e2fc1c000149c17a39a4b054fc7f
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-12-03 19:00:20 +0100

    improve makefile

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 16:19:20 by meghribe          #+#    #+#             */
-/*   Updated: 2025/12/03 16:57:19 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/12/03 18:59:27 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,30 @@
 
 ClapTrap::ClapTrap(): name("Default"), hit_points(10), energy_points(10), attack_damage(0)
 {
-		std::cout << "Default constructor called" << std::endl;
+	std::cout << "Default constructor called" << std::endl;
 }
 
 ClapTrap::ClapTrap(std::string name): name(name), hit_points(10), energy_points(10), attack_damage(0)
 {
-		std::cout << "ClapTrap with name parameter constructor called" << std::endl;
+	std::cout << "ClapTrap with name parameter constructor called" << std::endl;
 }
 
-ClapTrap::ClapTrap(const ClapTrap& rhs): name(rhs.name), hit_points(rhs.hit_points), energy_points(rhs.energy_points), attack_damage(rhs.attack_damage)
+ClapTrap::ClapTrap(const ClapTrap& rhs): 
+	name(rhs.name), 
+	hit_points(rhs.hit_points), 
+	energy_points(rhs.energy_points), 
+	attack_damage(rhs.attack_damage)
 {
-		std::cout << "Copy constructor called" << std::endl;
+	std::cout << "Copy constructor called" << std::endl;
 }
 
 ClapTrap& ClapTrap::operator=(const ClapTrap& rhs)
 {
-		std::cout << "Copy assignment operator called" << std::endl;
-		if (this != &rhs)
-		{
-				this->name = rhs.name;
-		}
-		return (*this);
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &rhs)
+	{
+		// TODO: ADD MORE
+		this->name = rhs.name;
+	}
+	return (*this);
 }
